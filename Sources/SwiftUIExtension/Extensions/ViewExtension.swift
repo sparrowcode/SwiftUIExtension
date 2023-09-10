d64b4bfaa1ef15efb431eaee37a50940b8a61e32
Ivan Vorobei <hello@ivanvorobei.io>
2023-09-10 22:46:00 +0300

    Added observer modifier and rotate modifier.

---
+++ Sources/SwiftUIExtension/Extensions/ViewExtension.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+extension View {
+    
+    public func onReceive(notification name: Notification.Name, perform action: @escaping (NotificationCenter.Publisher.Output) -> Void) -> some View {
+        self.onReceive(NotificationCenter.default.publisher(for: name), perform: action)
+    }
+    
+    public func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
+        self.modifier(DeviceRotationViewModifier(action: action))
+    }
+}
+
+struct DeviceRotationViewModifier: ViewModifier {
+    
+    let action: (UIDeviceOrientation) -> Void
+    
+    func body(content: Content) -> some View {
+        content
+            .onAppear()
+            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+                action(UIDevice.current.orientation)
+            }
+    }
+}
