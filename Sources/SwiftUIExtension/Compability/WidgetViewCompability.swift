b6ea9bb1b3c1d7a006da23e51ab9a1448635135c
Ivan Vorobei <hello@ivanvorobei.io>
2023-09-13 11:52:23 +0300

    Restructed.

--- Sources/SwiftUIExtension/Compability/WidgetViewCompability.swift
+++ Sources/SwiftUIExtension/Compability/WidgetViewCompability.swift
@@ -6,6 +6,10 @@ extension View {
     public func containerBackgroundForWidgetCompability<Background>(background: @escaping () -> Background) -> some View where Background: View {
         modifier(ContainerBackgroundForWidgetCompabilityModifier(background: background))
     }
+
+    public func containerBackgroundForWidget<Background>(@ViewBuilder background: @escaping () -> Background) -> some View where Background: View {
+        modifier(ContainerBackgroundForWidgetModifier(background: background))
+    }
 }
 
 struct ContainerBackgroundForWidgetCompabilityModifier<Background>: ViewModifier where Background: View {
@@ -20,6 +24,26 @@ struct ContainerBackgroundForWidgetCompabilityModifier<Background>: ViewModifier
                 }
         } else {
             content
+        }
+    }
+}
+
+struct ContainerBackgroundForWidgetModifier<Background>: ViewModifier where Background: View {
+    
+    let background: () -> Background
+    
+    func body(content: Content) -> some View {
+        if #available(iOS 17.0, iOSApplicationExtension 17.0, watchOS 10.0, watchOSApplicationExtension 10.0, macOS 14.0, *) {
+            content
+                .containerBackground(for: .widget) {
+                    background()
+                }
+        } else {
+            ZStack {
+                background()
+                content
+                    .padding()
+            }
         }
     }
 }
--- Sources/SwiftUIExtension/Extensions/ViewExtension.swift
+++ Sources/SwiftUIExtension/Extensions/ViewExtension.swift
@@ -5,30 +5,6 @@ extension View {
     public func onReceive(notification name: Notification.Name, perform action: @escaping (NotificationCenter.Publisher.Output) -> Void) -> some View {
         self.onReceive(NotificationCenter.default.publisher(for: name), perform: action)
     }
-    
-    public func containerBackgroundForWidget<Background>(@ViewBuilder background: @escaping () -> Background) -> some View where Background: View {
-        modifier(ContainerBackgroundForWidgetModifier(background: background))
-    }
-}
-
-struct ContainerBackgroundForWidgetModifier<Background>: ViewModifier where Background: View {
-    
-    let background: () -> Background
-    
-    func body(content: Content) -> some View {
-        if #available(iOS 17.0, iOSApplicationExtension 17.0, watchOS 10.0, watchOSApplicationExtension 10.0, macOS 14.0, *) {
-            content
-                .containerBackground(for: .widget) {
-                    background()
-                }
-        } else {
-            ZStack {
-                background()
-                content
-                    .padding()
-            }
-        }
-    }
 }
 
 // MARK: - Only iOS
