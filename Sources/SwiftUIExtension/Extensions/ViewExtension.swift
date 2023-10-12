94cf28ae360b39c61bdd7a61bcd0a54d4c4d3e1b
Ivan Vorobei <hello@ivanvorobei.io>
2023-10-13 01:46:13 +0300

    Fixed access level for condition modifier.

--- Sources/SwiftUIExtension/Extensions/ViewExtension.swift
+++ Sources/SwiftUIExtension/Extensions/ViewExtension.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 extension View {
     
-    @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+    @ViewBuilder public func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
         if condition {
             transform(self)
         } else {
