f07747cb21dc2334e87f2440550f32629313243e
Ivan Vorobei <hello@ivanvorobei.io>
2024-01-15 13:12:18 +0300

    Update ViewExtension+LayoutMargins.swift

--- Sources/SwiftUIExtension/Extensions/ViewExtension+LayoutMargins.swift
+++ Sources/SwiftUIExtension/Extensions/ViewExtension+LayoutMargins.swift
@@ -24,19 +24,10 @@ struct HorizontalSystemPadding: ViewModifier {
         }
     }
     
-    //var withCompensation: Bool = false
-    
     @Environment(\.horizontalSizeClass) var horizontalSizeClass
     
     public func body(content: Content) -> some View {
-        /*let compensation: CGFloat = {
-            #if os(visionOS)
-            return -14
-            #else
-            return .zero
-            #endif
-        }()*/
-        if #available(iOS 17.0, macOS 14.0, *) {
+        if #available(iOS 17.0, macOS 14.0, watchOS 10.0, *) {
             content
                 .contentMargins(.horizontal, value, for: .scrollContent)
         } else {
