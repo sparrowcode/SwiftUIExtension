810994f9d80880314ae96b13b71fe0acdc49978f
Ivan Vorobei <hello@ivanvorobei.io>
2025-05-21 11:42:22 +0300

    Update ViewCompability.swift

--- Sources/SwiftUIExtension/Compability/ViewCompability.swift
+++ Sources/SwiftUIExtension/Compability/ViewCompability.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import SwiftUIIntrospect
 
 extension View {
     
@@ -19,12 +20,12 @@ extension View {
     }
     
     /*public func fontWeightCompability(_ weight: Font.Weight) -> some View {
-        if #available(iOS 16.0, watchOS 9.0, macOS 13.0, *) {
-            return self.fontWeight(weight)
-        } else {
-            return self
-        }
-    }*/
+     if #available(iOS 16.0, watchOS 9.0, macOS 13.0, *) {
+     return self.fontWeight(weight)
+     } else {
+     return self
+     }
+     }*/
     
     public func invalidatableContentCompability() -> some View {
         if #available(iOS 17.0, watchOS 10.0, macOS 14.0, *) {
@@ -46,9 +47,13 @@ extension View {
         if #available(iOS 17.0, watchOS 10.0, macOS 14.0, *) {
             return self.scrollClipDisabled(disabled)
         } else {
+            #if !os(watchOS)
             return self.introspect(.scrollView, on: .iOS(.v15, .v16)) { scrollView in
-                    scrollView.clipsToBounds = false
-                }
+                scrollView.clipsToBounds = false
+            }
+            #else
+            return self
+            #endif
         }
     }
     
