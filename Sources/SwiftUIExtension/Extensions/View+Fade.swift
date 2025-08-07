80d0d0f783a49a9cbb3a827e8ca311b992b5e55d
Ivan Vorobei <hello@ivanvorobei.io>
2025-08-07 18:24:10 +0400

    Update View+Fade.swift

--- Sources/SwiftUIExtension/Extensions/View+Fade.swift
+++ Sources/SwiftUIExtension/Extensions/View+Fade.swift
@@ -2,38 +2,87 @@ import SwiftUI
 
 extension View {
     
-    public func fade(top: CGFloat? = nil, bottom: CGFloat? = nil) -> some View {
-        modifier(VerticalFade(top: top, bottom: bottom))
+    public func fade(
+        top: CGFloat? = nil,
+        bottom: CGFloat? = nil,
+        leading: CGFloat? = nil,
+        trailing: CGFloat? = nil
+    ) -> some View {
+        modifier(
+            Fade(
+                top: top,
+                bottom: bottom,
+                leading: leading,
+                trailing: trailing
+            )
+        )
+    }
+    
+    public func fade(
+        horizontal: CGFloat? = nil,
+        vertical: CGFloat? = nil
+    ) -> some View {
+        modifier(
+            Fade(
+                top: vertical,
+                bottom: vertical,
+                leading: horizontal,
+                trailing: horizontal
+            )
+        )
     }
 }
 
-struct VerticalFade: ViewModifier {
+struct Fade: ViewModifier {
     
     let top: CGFloat?
     let bottom: CGFloat?
+    let leading: CGFloat?
+    let trailing: CGFloat?
     
     func body(content: Content) -> some View {
         content
-             .mask {
-                 VStack(spacing: .zero) {
-                     
-                     if let top {
-                         // Top Fade
-                         LinearGradient(colors: [Color.black.opacity(0), Color.black], startPoint: .top, endPoint: .bottom)
-                             .frame(height: top)
-                     }
-                     
-                     // Middle
-                     Rectangle()
-                         .fill(Color.black)
-                         .ignoresSafeArea()
-                     
-                     if let bottom {
-                         // Bottom Fade
-                         LinearGradient(colors: [Color.black, Color.black.opacity(0)], startPoint: .top, endPoint: .bottom)
-                             .frame(height: bottom)
-                     }
-                 }
+            .mask {
+                VStack(spacing: .zero) {
+                    
+                    // Top Fade
+                    if let top {
+                        LinearGradient(colors: [Color.black.opacity(0), Color.black], startPoint: .top, endPoint: .bottom)
+                            .frame(height: top)
+                    }
+                    
+                    // Middle
+                    Rectangle()
+                        .fill(Color.black)
+                        .ignoresSafeArea()
+                    
+                    // Bottom Fade
+                    if let bottom {
+                        LinearGradient(colors: [Color.black, Color.black.opacity(0)], startPoint: .top, endPoint: .bottom)
+                            .frame(height: bottom)
+                    }
+                }
+                .mask {
+                    HStack(spacing: .zero) {
+                        
+                        // Top Fade
+                        if let leading {
+                            LinearGradient(colors: [Color.black.opacity(0), Color.black], startPoint: .leading, endPoint: .trailing)
+                                .frame(width: leading)
+                        }
+                        
+                        // Middle
+                        Rectangle()
+                            .fill(Color.black)
+                            .ignoresSafeArea()
+                        
+                        // Bottom Fade
+                        if let trailing {
+                            LinearGradient(colors: [Color.black, Color.black.opacity(0)], startPoint: .leading, endPoint: .trailing)
+                                .frame(width: trailing)
+                        }
+                    }
+                }
              }
     }
 }
